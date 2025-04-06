1f816664e0f478a3eec38d7f29f23990262f0d1f
Kalashnikov-AN <huumaannnnn@gmail.com>
2025-04-06 18:03:53 +0900

    differentiation

---
+++ differentiation/firstMethod/differentitation.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <cmath>
+#include <string>
+#include <locale>
+using namespace std;
+
+double f(double x) {
+    return 2 * (sin(x));
+}
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+
+    const double Pi = 3.141592654;
+    int M1 = 2;
+    int M2 = 2;
+    double xo = Pi / 4;
+    double eps = 0.001;
+
+    string s1 = "По первой формуле верхний порог для h = ";
+    s1 += to_string(sqrt(6 * eps / M1)) + ". Введите желаемое h...";
+    double h1;
+    cout << s1 << endl;
+    cin >> h1;
+    double diff1 = (f(xo + h1) - f(xo - h1)) / (2 * h1);
+    cout << "Производная, вычисленная по первой формуле: " << diff1 << endl;
+
+    string s2 = "По второй формуле верхний порог для h = ";
+    s2 += to_string(pow(30 * eps / M2, 0.25)) + ". Введите желаемое h...";
+    double h2;
+    cout << s2 << endl;
+    cin >> h2;
+    double diff2 = (f(xo - 2 * h2) - 8 * f(xo - h2) + 8 * f(xo + h2) - f(xo + 2 * h2)) / (12 * h2);
+    cout << "Производная, вычисленная по второй формуле: " << diff2 << endl;
+
+    return 0;
+}
---
+++ differentiation/secondMethod/secondMethod.cpp
@@ -0,0 +1,59 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <vector>
+#include <locale>
+using namespace std;
+
+int main() {
+
+    setlocale(LC_ALL, "Russian");
+
+    const int n = 5;
+    vector<double> x(n + 1);
+    vector<vector<double>> y(n + 1, vector<double>(n + 1));
+    double h = 0.1;
+
+    x[0] = 0.2;
+    x[1] = 0.4;
+    x[2] = 0.6;
+    x[3] = 0.8;
+    x[4] = 1.0;
+    x[5] = 1.2;
+
+    y[0][0] = 1.2214;
+    y[1][0] = 1.4918;
+    y[2][0] = 1.8221;
+    y[3][0] = 2.2255;
+    y[4][0] = 2.7183;
+    y[5][0] = 3.3201;
+   
+    cout << setw(5) << "k" << setw(10) << "x" << setw(10) << "y(x)" << endl;
+
+    for (int i = 0; i <= n; ++i) {
+        cout << setw(5) << i << setw(10) << x[i] << setw(10) << y[i][0] << endl;
+    }
+
+    for (int j = 1; j <= n; ++j) {
+        cout << "D" << j << endl;
+        for (int i = 0; i <= n - j; ++i) {
+            y[i][j] = y[i + 1][j - 1] - y[i][j - 1];
+            cout << setw(10) << y[i][j] << endl;
+        }
+        cout << endl;
+    }
+
+    cout << fixed << setprecision(5) << setw(10) << "y'(xo)" << fixed << setprecision(5) << setw(20) << "погр." << endl;
+
+    double s = 0;
+    double d = -1;
+    double Delta = 0;
+
+    for (int j = 1; j < n; ++j) {
+        d = -d;
+        s += d * y[0][j] / (j * h);
+        Delta = abs(y[0][j + 1] / ((j + 1) * h));
+        cout << setw(10) << s << setw(20) << Delta << endl;
+    }
+
+}
